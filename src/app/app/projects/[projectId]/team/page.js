f051cebab86a24f76f1097d73a85f0e602b2400d
pranav605 <pranav605@gmail.com>
2025-06-27 14:35:34 -0400

    Added teams route

---
+++ src/app/app/projects/[projectId]/team/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Team() {
+  return (
+    <div className='w-full h-full'>
+        <div className='flex flex-col'>
+            <h1 className='text-4xl font-extrabold'>Team</h1>
+        </div>
+    </div>
+  )
+}
